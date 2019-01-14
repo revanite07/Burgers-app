9291fced8b9b635b15b8680f5fb56eceaa8c909b
Kelan Moffatt <kelanmoffatt07@gmail.com>
2019-01-14 10:13:55 -0800

    first

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -2,10 +2,10 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
+
 var burger = require("../models/burgers.js");
 
-// Create all our routes and set up logic within those routes where required.
+
 router.get("/", function(req, res) {
   burger.all(function(data) {
     var hbsObject = {
@@ -22,7 +22,7 @@ router.post("/api/burgers", function(req, res) {
   ], [
     req.body.name, req.body.devour
   ], function(result) {
-    // Send back the ID of the new quote
+  
     res.json({ id: result.insertId });
   });
 });
@@ -36,7 +36,7 @@ router.put("/api/burgers/:id", function(req, res) {
     devour: req.body.devour
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+    
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -49,7 +49,7 @@ router.delete("/api/burgers/:id", function(req, res) {
 
   burger.delete(condition, function(result) {
     if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
+  
       return res.status(404).end();
     } else {
       res.status(200).end();
@@ -57,5 +57,5 @@ router.delete("/api/burgers/:id", function(req, res) {
   });
 });
 
-// Export routes for server.js to use.
+
 module.exports = router;
